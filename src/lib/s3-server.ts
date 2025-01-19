67de74f6f8442b0b78d3b31e14ca6fb0a859cc7f
Geeten <geeten2301@gmail.com>
2025-01-19 19:48:00 +0530

    minor fixes

--- src/lib/s3-server.ts
+++ src/lib/s3-server.ts
@@ -7,12 +7,12 @@ export async function downloadFromS3(file_key: string): Promise<string> {
       const s3 = new S3({
         region: "ap-south-1",
         credentials: {
-          accessKeyId: process.env.S3_ACCESS_KEY_ID!,
-          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY_ID!,
+          accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY_ID!,
         },
       });
       const params = {
-        Bucket: process.env.S3_BUCKET_NAME!,
+        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
         Key: file_key,
       };
             // @ts-ignore
@@ -39,46 +39,3 @@ export async function downloadFromS3(file_key: string): Promise<string> {
     }
   });
 }
-
-// downloadFromS3("uploads/1693568801787chongzhisheng_resume.pdf");
-
-
-
-// import AWS from "aws-sdk";
-// import fs from "fs";
-
-// export async function downloadFromS3(file_key:string){
-
-//     try {
-//             AWS.config.update({
-//                 accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
-//                 secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY_ID
-//             })
-    
-//             const s3 = new AWS.S3({
-//                 params:{
-//                     Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
-//                 },
-//                 region:'ap-south-1'
-//             })
-    
-           
-//             const params = {
-//                 Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
-//                 Key: file_key,
-//               };
-    
-//               const obj = await s3.getObject(params).promise();
-
-
-//                 const file_name  = `/tmp/pdf-${Date.now()}.pdf`
-
-//               fs.writeFileSync(file_name ,obj.Body as Buffer)
-
-//               return file_name;
-            
-    
-//         } catch (error) {
-//             console.log(error)
-//         }
-// }
--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -8,8 +8,8 @@ export async function uploadtoS3(
       const s3 = new S3({
         region: "ap-south-1",
         credentials: {
-          accessKeyId: process.env.S3_ACCESS_KEY_ID!,
-          secretAccessKey: process.env.S3_SECRET_ACCESS_KEY_ID!,
+          accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID!,
+          secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY_ID!,
         },
       });
 
@@ -17,7 +17,7 @@ export async function uploadtoS3(
         "uploads/" + Date.now().toString() + file.name.replace(" ", "-");
 
       const params = {
-        Bucket: process.env.S3_BUCKET_NAME!,
+        Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
         Key: file_key,
         Body: file,
       };
@@ -37,54 +37,10 @@ export async function uploadtoS3(
 }
 
 
-// import AWS from "aws-sdk";
-
-
-// export async function uploadtoS3(file: File){
-
-//     try {
-//         AWS.config.update({
-//             accessKeyId: process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID,
-//             secretAccessKey: process.env.NEXT_PUBLIC_S3_SECRET_ACCESS_KEY_ID
-//         })
-
-//         const s3 = new AWS.S3({
-//             params:{
-//                 Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME,
-//             },
-//             region:'ap-south-1'
-//         })
- 
-//         const file_key =
-//         "uploads/" + Date.now().toString() + file.name.replace(" ", "-");
-
-//         const params = {
-//             Bucket: process.env.NEXT_PUBLIC_S3_BUCKET_NAME!,
-//             Key: file_key,
-//             Body: file,
-//           };
-
-//           const upload = s3.putObject(params).on('httpUploadProgress',evt =>{
-//             console.log("uploading to s3",parseInt(((evt.loaded*100)/evt.total).toString()) + " %")
-//           }).promise()
-
-//           await upload.then(data=>{
-//             console.log("succes",file_key)
-//           })
-
-//           return Promise.resolve({
-//             file_key,
-//             file_name:file.name
-//           })
-
-//     } catch (error) {
-//       console.log(error)
-//     }
-// } 
 
 
 export function getS3Url(file_key: string) {
-    const url = `https://${process.env.S3_BUCKET_NAME}.s3.ap-south-1.amazonaws.com/${file_key}`;
+    const url = `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME}.s3.ap-south-1.amazonaws.com/${file_key}`;
     return url;
   }
 
